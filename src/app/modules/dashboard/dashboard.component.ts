d839d4e3eaacaf65b7f719a519fe7c5747af9409
RajKamal SR <rajkamal.sr@pitsolutions.com>
2018-11-14 16:58:34 +0530

    tinyMCE editor integration

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+// import "tinymce";
+// import "tinymce/themes/modern";
+// import "tinymce/plugins/table";
+// import "tinymce/plugins/link";
+// import "tinymce/plugins/textcolor";
+// import "tinymce/plugins/lists";
+// import "tinymce/plugins/advlist";
+// import "tinymce/plugins/media";
+// import "tinymce/plugins/image";
+// import "tinymce/plugins/anchor";
+// import "tinymce/plugins/insertdatetime";
+// import "tinymce/plugins/charmap";
+// declare var tinymce: any;
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
- 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-
+    //this.tinyMceInit();
   }
 
+  private tinyMceInit(): void {
+    // tinymce.init({
+    //   allow_html_in_named_anchor: true,
+    //   init_instance_callback: function(editor) {
+    //     editor.focus();
+    //   },
+    //   height: 200,
+    //   toolbar: `formatselect | undo redo | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | insert | help`
+    // });
+  }
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,12 +1,15 @@
 import { DashboardComponents } from './dashboard.include';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EditorModule } from '@tinymce/tinymce-angular';
+// import { NgxEditorModule } from 'ngx-editor';
 
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    EditorModule
   ],
   declarations: [...DashboardComponents],
   exports: [...DashboardComponents]
